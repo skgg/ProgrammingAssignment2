cafc28ed705f0a3068ef6fc32e2042daa65f3dc8
skg <skg@skg-MBA.local>
2014-12-20 18:59:50 +0800

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The first function, makeCacheMatrix creates a special "matrix",
+#which is really a list containing a function to
+#1. set the value of the matrix
+#2. get the value of the matrix
+#3. set the value of the inversion
+#4. get the value of the inversion
 
-## Write a short comment describing this function
+#The second function calculates the inversion of the special "matrix" created with the above function.
+#However, it first checks to see if the inversion has already been calculated. If so, it gets the inversion from the cache and skips the computation.
+#Otherwise, it calculates the inversion of the data and sets the value of the inversion in the cache via the setinversion function.
 
+#creates a special "matrix" and implement the functions
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function (y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+#cache the inversion if it has calculated and the matrix hasn't been modify, otherwise, use solve() to calculate it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if (!is.null(inv)) {
+        message("getting cached data");
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
